888a91ad67d8a5eeb6fd9443fbc269003aea8de9
Jake Walker <j.walker@tribetechgroup.com>
2023-12-15 20:32:49 +0000

    Day 5 Part 1 incomplete

---
+++ day_5/seed_fertilizer.py
@@ -0,0 +1,30 @@
+# Top row = Seeds to be planted
+
+# each map contains:
+    # destination range start
+    # source range start
+    # range length
+
+# Consider
+
+# Destination    Source      Range
+#   Range         Range      Length
+#    50             98          2
+
+
+# Range length of 2 means:
+
+# Destination    Source            
+# Range          Range
+# 50 -> 51      98 -> 99
+
+
+# Therefore (Seed    ->    Soil)
+#             98     ->     50 
+#             99     ->     51
+
+
+'''
+_______________________________________________________________________________
+'''
+
